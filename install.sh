3f309bd637979c81dd61bbefc04e61536d4c809d
jichang <jimmy.chang@amd.com>
2022-11-07 16:32:59 +0800

    Add the appropriate copyright
    [ROCm/hipBLASLt commit: 212d9615aab6ed55e1179054dd4002e211d58ff3]

--- install.sh
+++ install.sh
@@ -1,5 +1,26 @@
 #!/usr/bin/env bash
-# Author: jichang@amd.com
+
+# ########################################################################
+# Copyright (c) 2022 Advanced Micro Devices, Inc. All rights reserved.
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell cop-
+# ies of the Software, and to permit persons to whom the Software is furnished
+# to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IM-
+# PLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+# FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+# COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+# IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNE-
+# CTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+#
+# ########################################################################
 
 #set -x #echo on
 
@@ -8,7 +29,7 @@
 # #################################################
 function display_help()
 {
-  echo "rocBLASLt build & installation helper script"
+  echo "hipBLASLt build & installation helper script"
   echo "./install [-h|--help] "
   echo "    [-h|--help] prints this help message"
 #  echo "    [--prefix] Specify an alternate CMAKE_INSTALL_PREFIX for cmake"
