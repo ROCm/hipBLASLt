655aea8aea57f81d754b7108001279adef6f9439
jichang <jimmy.chang@amd.com>
2022-12-14 11:46:37 +0800

    Correct the Copyright (c) as (C)
    [ROCm/hipBLASLt commit: ea59c66d64d86712ab3257d3281807022f5cee97]

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # ########################################################################
-# Copyright (c) 2022 Advanced Micro Devices, Inc. All rights reserved.
+# Copyright (C) 2022 Advanced Micro Devices, Inc. All rights reserved.
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
