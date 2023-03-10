ab0fd24c16b292f12d28b1e5dab85e28f606b81d
victorwu <Jinp800125@gmail.com>
2023-03-10 01:53:22 -0800

    BF16 rounding instead truncate
    [ROCm/hipBLASLt commit: 2b096853be41e278e5243499cf45b210d0448d4d]

--- tensilelite/Tensile/Source/lib/include/Tensile/DataTypes_BFloat16.hpp
+++ tensilelite/Tensile/Source/lib/include/Tensile/DataTypes_BFloat16.hpp
@@ -115,7 +115,7 @@ namespace Tensile
         }
 
         // truncate lower 16 bits of IEEE float to convert to bfloat16
-        static BFloat16 float_to_bfloat16(const float v)
+        static BFloat16 float_to_bfloat16_truncate(const float v)
         {
             BFloat16 bf16;
             if(std::isnan(v))
@@ -135,6 +135,57 @@ namespace Tensile
 #else
             bf16.data = p[1];
 #endif
+            return bf16;
+        }
+
+        static BFloat16 float_to_bfloat16(const float v)
+        {
+            BFloat16 bf16;
+            if(std::isnan(v))
+            {
+                bf16.data = BFLOAT16_Q_NAN_VALUE;
+                return bf16;
+            }
+            union
+            {
+                float    fp32;
+                uint32_t int32;
+            };
+            fp32 = v;
+
+            if(~int32 & 0x7f800000)
+            {
+                // When the exponent bits are not all 1s, then the value is zero, normal,
+                // or subnormal. We round the bfloat16 mantissa up by adding 0x7FFF, plus
+                // 1 if the least significant bit of the bfloat16 mantissa is 1 (odd).
+                // This causes the bfloat16's mantissa to be incremented by 1 if the 16
+                // least significant bits of the float mantissa are greater than 0x8000,
+                // or if they are equal to 0x8000 and the least significant bit of the
+                // bfloat16 mantissa is 1 (odd). This causes it to be rounded to even when
+                // the lower 16 bits are exactly 0x8000. If the bfloat16 mantissa already
+                // has the value 0x7f, then incrementing it causes it to become 0x00 and
+                // the exponent is incremented by one, which is the next higher FP value
+                // to the unrounded bfloat16 value. When the bfloat16 value is subnormal
+                // with an exponent of 0x00 and a mantissa of 0x7F, it may be rounded up
+                // to a normal value with an exponent of 0x01 and a mantissa of 0x00.
+                // When the bfloat16 value has an exponent of 0xFE and a mantissa of 0x7F,
+                // incrementing it causes it to become an exponent of 0xFF and a mantissa
+                // of 0x00, which is Inf, the next higher value to the unrounded value.
+                int32 += 0x7fff + ((int32 >> 16) & 1); // Round to nearest, round to even
+            }
+            else if(int32 & 0xffff)
+            {
+                // When all of the exponent bits are 1, the value is Inf or NaN.
+                // Inf is indicated by a zero mantissa. NaN is indicated by any nonzero
+                // mantissa bit. Quiet NaN is indicated by the most significant mantissa
+                // bit being 1. Signaling NaN is indicated by the most significant
+                // mantissa bit being 0 but some other bit(s) being 1. If any of the
+                // lower 16 bits of the mantissa are 1, we set the least significant bit
+                // of the bfloat16 mantissa, in order to preserve signaling NaN in case
+                // the bloat16's mantissa bits are all 0.
+                int32 |= 0x10000; // Preserve signaling NaN
+            }
+            bf16.data = int32 >> 16;
             return bf16;
         }
     };
