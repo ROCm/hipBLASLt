7b0dcb4baf366bb464cf6ab5b0ad734c3fc173cf
YangWen Huang <yangwen.huang@amd.com>
2023-08-30 10:25:08 +0800

    Add simpleGemmAlphaVecExt

--- clients/samples/common/helper.h
+++ clients/samples/common/helper.h
@@ -77,11 +77,13 @@ struct Runner
         CHECK_HIP_ERROR(hipMalloc(&d_b, n * k * batch_count * sizeof(InType)));
         CHECK_HIP_ERROR(hipMalloc(&d_c, m * n * batch_count * sizeof(OutType)));
         CHECK_HIP_ERROR(hipMalloc(&d_d, m * n * batch_count * sizeof(OutType)));
+        CHECK_HIP_ERROR(hipMalloc(&d_alphaVec, m * batch_count * sizeof(float)));
 
         CHECK_HIP_ERROR(hipHostMalloc(&a, m * k * batch_count * sizeof(InType)));
         CHECK_HIP_ERROR(hipHostMalloc(&b, n * k * batch_count * sizeof(InType)));
         CHECK_HIP_ERROR(hipHostMalloc(&c, m * n * batch_count * sizeof(OutType)));
         CHECK_HIP_ERROR(hipHostMalloc(&d, m * n * batch_count * sizeof(OutType)));
+        CHECK_HIP_ERROR(hipHostMalloc(&alphaVec, m * batch_count * sizeof(float)));
 
         if(max_workspace_size > 0)
             CHECK_HIP_ERROR(hipMalloc(&d_workspace, max_workspace_size));
@@ -91,7 +93,9 @@ struct Runner
         for(int i = 0; i < n * k * batch_count; i++)
             ((InType*)b)[i] = static_cast<InType>((rand() % 7) - 3);
         for(int i = 0; i < m * n * batch_count; i++)
-            ((InType*)c)[i] = static_cast<OutType>((rand() % 7) - 3);
+            ((OutType*)c)[i] = static_cast<OutType>((rand() % 7) - 3);
+        for(int i = 0; i < m * batch_count; ++i)
+            ((float*)alphaVec)[i] = static_cast<float>((rand() % 7) - 3);
     }
 
     ~Runner()
@@ -101,10 +105,12 @@ struct Runner
         CHECK_HIP_ERROR(hipFree(b));
         CHECK_HIP_ERROR(hipFree(c));
         CHECK_HIP_ERROR(hipFree(d));
+        CHECK_HIP_ERROR(hipFree(alphaVec));
         CHECK_HIP_ERROR(hipFree(d_a));
         CHECK_HIP_ERROR(hipFree(d_b));
         CHECK_HIP_ERROR(hipFree(d_c));
         CHECK_HIP_ERROR(hipFree(d_d));
+        CHECK_HIP_ERROR(hipFree(d_alphaVec));
         CHECK_HIPBLASLT_ERROR(hipblasLtDestroy(handle));
         CHECK_HIP_ERROR(hipStreamDestroy(stream));
     }
@@ -117,6 +123,8 @@ struct Runner
             d_b, b, n * k * batch_count * sizeof(InType), hipMemcpyHostToDevice, stream));
         CHECK_HIP_ERROR(hipMemcpyAsync(
             d_c, c, m * n * batch_count * sizeof(OutType), hipMemcpyHostToDevice, stream));
+        CHECK_HIP_ERROR(hipMemcpyAsync(
+            d_alphaVec, alphaVec, m * batch_count * sizeof(float), hipMemcpyHostToDevice, stream));
     }
 
     void deviceToHost()
@@ -142,8 +150,8 @@ struct Runner
     AlphaType alpha;
     BetaType  beta;
 
-    void *a, *b, *c, *d; // host
-    void *d_a, *d_b, *d_c, *d_d; // device
+    void *a, *b, *c, *d, *alphaVec; // host
+    void *d_a, *d_b, *d_c, *d_d, *d_alphaVec; // device
 
     void*   d_workspace;
     int64_t max_workspace_size;
@@ -176,28 +184,34 @@ struct RunnerVec
         d_b.resize(m.size(), nullptr);
         d_c.resize(m.size(), nullptr);
         d_d.resize(m.size(), nullptr);
+        d_alphaVec.resize(m.size(), nullptr);
         a.resize(m.size(), nullptr);
         b.resize(m.size(), nullptr);
         c.resize(m.size(), nullptr);
         d.resize(m.size(), nullptr);
+        alphaVec.resize(m.size(), nullptr);
         for(int j = 0; j < m.size(); j++)
         {
             CHECK_HIP_ERROR(hipMalloc(&d_a[j], m[j] * k[j] * batch_count[j] * sizeof(InType)));
             CHECK_HIP_ERROR(hipMalloc(&d_b[j], n[j] * k[j] * batch_count[j] * sizeof(InType)));
             CHECK_HIP_ERROR(hipMalloc(&d_c[j], m[j] * n[j] * batch_count[j] * sizeof(OutType)));
             CHECK_HIP_ERROR(hipMalloc(&d_d[j], m[j] * n[j] * batch_count[j] * sizeof(OutType)));
+            CHECK_HIP_ERROR(hipMalloc(&d_alphaVec[j], m[j] * batch_count[j] * sizeof(float)));
 
             CHECK_HIP_ERROR(hipHostMalloc(&a[j], m[j] * k[j] * batch_count[j] * sizeof(InType)));
             CHECK_HIP_ERROR(hipHostMalloc(&b[j], n[j] * k[j] * batch_count[j] * sizeof(InType)));
             CHECK_HIP_ERROR(hipHostMalloc(&c[j], m[j] * n[j] * batch_count[j] * sizeof(OutType)));
             CHECK_HIP_ERROR(hipHostMalloc(&d[j], m[j] * n[j] * batch_count[j] * sizeof(OutType)));
+            CHECK_HIP_ERROR(hipHostMalloc(&alphaVec[j], m[j] * batch_count[j] * sizeof(float)));
 
             for(int i = 0; i < m[j] * k[j] * batch_count[j]; i++)
                 ((InType*)a[j])[i] = static_cast<InType>((rand() % 7) - 3);
             for(int i = 0; i < n[j] * k[j] * batch_count[j]; i++)
                 ((InType*)b[j])[i] = static_cast<InType>((rand() % 7) - 3);
             for(int i = 0; i < m[j] * n[j] * batch_count[j]; i++)
-                ((InType*)c[j])[i] = static_cast<OutType>((rand() % 7) - 3);
+                ((OutType*)c[j])[i] = static_cast<OutType>((rand() % 7) - 3);
+            for(int i = 0; i < m[j] * batch_count[j]; i++)
+                ((float*)alphaVec[j])[i] = static_cast<float>((rand() % 7) - 3);
         }
         if(max_workspace_size > 0)
             CHECK_HIP_ERROR(hipMalloc(&d_workspace, max_workspace_size));
@@ -211,10 +225,12 @@ struct RunnerVec
             CHECK_HIP_ERROR(hipFree(b[j]));
             CHECK_HIP_ERROR(hipFree(c[j]));
             CHECK_HIP_ERROR(hipFree(d[j]));
+            CHECK_HIP_ERROR(hipFree(alphaVec[j]));
             CHECK_HIP_ERROR(hipFree(d_a[j]));
             CHECK_HIP_ERROR(hipFree(d_b[j]));
             CHECK_HIP_ERROR(hipFree(d_c[j]));
             CHECK_HIP_ERROR(hipFree(d_d[j]));
+            CHECK_HIP_ERROR(hipFree(d_alphaVec[j]));
         }
         CHECK_HIP_ERROR(hipFree(d_workspace));
         CHECK_HIPBLASLT_ERROR(hipblasLtDestroy(handle));
@@ -238,6 +254,11 @@ struct RunnerVec
             CHECK_HIP_ERROR(hipMemcpyAsync(d_c[j],
                                            c[j],
                                            m[j] * n[j] * batch_count[j] * sizeof(OutType),
+                                           hipMemcpyHostToDevice,
+                                           stream));
+            CHECK_HIP_ERROR(hipMemcpyAsync(d_alphaVec[j],
+                                           alphaVec[j],
+                                           m[j] * batch_count[j] * sizeof(float),
                                            hipMemcpyHostToDevice,
                                            stream));
         }
@@ -272,8 +293,8 @@ struct RunnerVec
     std::vector<AlphaType> alpha;
     std::vector<BetaType>  beta;
 
-    std::vector<void*> a, b, c, d; // host
-    std::vector<void*> d_a, d_b, d_c, d_d; // device
+    std::vector<void*> a, b, c, d, alphaVec; // host
+    std::vector<void*> d_a, d_b, d_c, d_d, d_alphaVec; // device
 
     void*   d_workspace;
     int64_t max_workspace_size;
