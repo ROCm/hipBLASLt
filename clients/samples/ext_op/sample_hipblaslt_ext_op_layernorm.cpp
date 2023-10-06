87afa10c7908137900a6c2f3297dff2b1182be5d
yu-hsieh <Yu-cheng.Hsieh@amd.com>
2023-10-06 15:59:26 +0800

    layernorm sample

---
+++ clients/samples/ext_op/sample_hipblaslt_ext_op_layernorm.cpp
@@ -0,0 +1,85 @@
+/*******************************************************************************
+ *
+ * MIT License
+ *
+ * Copyright (C) 2022-2023 Advanced Micro Devices, Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ *******************************************************************************/
+
+#include <hip/hip_runtime.h>
+#include <hipblaslt/hipblaslt.h>
+#include <hipblaslt-ext-op.h>
+#include <iostream>
+
+#include "helper.h"
+
+void simpleAMax(hipblasltDatatype_t type,
+                   void*            d_out,
+                   void*            d_mean,
+                   void*            d_invvar,
+                   void*            d_in,
+                   int64_t          m,
+                   int64_t          n,
+                   float            eps,
+                   void*            d_gamma,
+                   void*            d_beta,
+                   hipStream_t      stream);
+
+int main()
+{
+    /** This is a amax example
+     *  in  = (m, n). lda = m
+     *  out = (1). ldb = 1
+     */
+    LayerNormRunner<float> runnerF32(135, 345);
+
+    runnerF32.run([&runnerF32] {
+        simpleAMax(HIPBLASLT_R_32F,
+                   runnerF32.d_out,
+                   runnerF32.d_mean,
+                   runnerF32.d_invvar,
+                   runnerF32.d_in,
+                   runnerF32.m,
+                   runnerF32.n,
+                   1e-5,
+                   runnerF32.d_gamma,
+                   runnerF32.d_beta,
+                   runnerF32.stream);
+    });
+
+    return 0;
+}
+
+void simpleAMax(hipblasltDatatype_t type,
+                   void*            d_out,
+                   void*            d_mean,
+                   void*            d_invvar,
+                   void*            d_in,
+                   int64_t          m,
+                   int64_t          n,
+                   float            eps,
+                   void*            d_gamma,
+                   void*            d_beta,
+                   hipStream_t      stream)
+{
+     CHECK_HIPBLASLT_ERROR(hipblasltExtLayerNorm(type, d_out, d_mean, d_invvar, d_in, m, n, eps, d_gamma, d_beta, stream));
+}
+
