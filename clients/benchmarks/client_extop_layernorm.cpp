313a639b16072628f11f27fd2a1b4bb96ddb6932
yu-hsieh <Yu-cheng.Hsieh@amd.com>
2023-10-06 15:59:26 +0800

    layernorm sample

--- clients/benchmarks/client_extop_layernorm.cpp
+++ clients/benchmarks/client_extop_layernorm.cpp
@@ -47,20 +47,6 @@ void printUsage(char* programName)
                  "hpl(floating). (default is hpl)\n";
 }
 
-static std::random_device                    rd;
-static std::mt19937                          gen(rd());
-static std::uniform_real_distribution<float> dist(-1.f, 1.f);
-
-template <typename Iter>
-void randomize(Iter beg, Iter end)
-{
-    using ValueType = typename std::remove_pointer<Iter>::type;
-    for(auto i = beg; i != end; ++i)
-    {
-        *i = dist(gen);
-    }
-}
-
 void cpuLayerNorm(float*        out,
                   float*        gpuMean,
                   float*        gpuInvvar,
