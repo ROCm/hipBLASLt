f9a39ee1e9b8a9e22de7574c66b0d3a0ecc0fae5
Vin Huang <vin.huang@amd.com>
2023-11-23 12:47:33 -0600

    [Tensilelite] using longBranchScc0 for GSU != 1 to jump over OptNLL when the use of Acc Vgpr is orver 100
    [ROCm/hipBLASLt commit: 700f1d624372abdfb22c8c8bff7bf3d0d2c1cd3f]

--- tensilelite/Tensile/KernelWriter.py
+++ tensilelite/Tensile/KernelWriter.py
@@ -2206,7 +2206,8 @@ class KernelWriter(metaclass=abc.ABCMeta):
       if not kernel["SuppressNoLoadLoop"]:
         gsuLabel = Label(label=self.labels.getNameInc("GSU"), comment="")
         module.add(SCmpEQU32(src0=sgpr("GSU"), src1=1, comment="GSU == 1 ?"))
-        module.add(SCBranchSCC0(labelName=gsuLabel.getLabelName(), comment="branch if GSU != 1"))
+        noLoadLoopModules = None
+        acclen = 0
         gsuBackup          = kernel["GlobalSplitU"]
         gsuAccumBackup     = kernel["_GlobalAccumulation"]
         bpeCexternalBackup = self.states.bpeCexternal
@@ -2226,11 +2227,20 @@ class KernelWriter(metaclass=abc.ABCMeta):
           self.saveLocalPointers(kernel, tensorParametersA, tensorParametersB)
           # deepCopy packCode for OptNLL noLoadLoop
           deepCopyPack = fastdeepcopy(pack)
-          module.add(self.noLoadLoop(kernel, tensorParametersA, tensorParametersB, isOptNLL=True, isNGLL=False, pack=deepCopyPack))
+          noLoadLoopModules = self.noLoadLoop(kernel, tensorParametersA, tensorParametersB, isOptNLL=True, isNGLL=False, pack=deepCopyPack)
+          acclen = noLoadLoopModules.countType(Instruction)
           self.restoreLocalPointers(kernel, tensorParametersA, tensorParametersB)
         kernel["GlobalSplitU"] = gsuBackup
         kernel["_GlobalAccumulation"] = gsuAccumBackup
         self.states.bpeCexternal = bpeCexternalBackup
+
+        if acclen > 16384:
+          module.add(self.longBranchScc0(gsuLabel, posNeg=1, comment="branch if GSU != 1"))
+        else:
+          module.add(SCBranchSCC0(labelName=gsuLabel.getLabelName(), comment="branch if GSU != 1"))
+        
+        if noLoadLoopModules != None:
+          module.add(noLoadLoopModules)
         module.add(gsuLabel)
         module.add(self.noLoadLoop(kernel, tensorParametersA, tensorParametersB, isOptNLL=False, isNGLL=False, pack=pack))
 
@@ -4250,6 +4260,13 @@ class KernelWriter(metaclass=abc.ABCMeta):
   ##############################################################################
   @abc.abstractmethod
   def functionSuffix(self, kernel):
+    return ""
+
+  ##############################################################################
+  # longBranchScc0 - 32 bit offset
+  ##############################################################################
+  @abc.abstractmethod
+  def longBranchScc0(self, label: Label, posNeg: int=0, comment=""):
     return ""
 
   ##############################################################################
