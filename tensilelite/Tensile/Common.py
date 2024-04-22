672cabbe0660d033adf8970875e7b59f91f7043a
AndySu12 <andysu12@amd.com>
2024-04-22 07:30:41 +0000

    Update description in Common.py

--- tensilelite/Tensile/Common.py
+++ tensilelite/Tensile/Common.py
@@ -70,7 +70,7 @@ globalParameters["MinFlopsPerSync"] = 1           # Minimum number of flops per 
 globalParameters["NumBenchmarks"] = 1             # how many benchmark data points to collect per problem/solution
 globalParameters["SyncsPerBenchmark"] = 1         # how iterations of the stream synchronization for-loop to do per benchmark data point
 globalParameters["EnqueuesPerSync"] = 1           # how many solution enqueues to perform per synchronization
-globalParameters["MaxEnqueuesPerSync"] = -1       # max solution enqueues to perform per synchronization. This factor will be restricted to 30ms if NumWarmups is set.
+globalParameters["MaxEnqueuesPerSync"] = -1       # max solution enqueues to perform per synchronization. This factor will be restricted if NumWarmups touches 30ms.
 globalParameters["SleepPercent"] = 300            # how long to sleep after every data point: 25 means 25% of solution time. Sleeping lets gpu cool down more.
 # cProfile
 globalParameters["Profiler"] = 0                  # Enable profiler. 0=off, 1=cProfile. This will set CpuThreads to 1.
