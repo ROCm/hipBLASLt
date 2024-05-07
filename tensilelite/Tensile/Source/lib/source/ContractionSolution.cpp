90f70c8162b018c2f17ae6008d6e890ae52a39a7
jichang <jimmy.chang@amd.com>
2024-05-07 09:11:48 +0000

    Refactor tensilelite problem predicate and remove some hard hypothesis from tensile_host

--- tensilelite/Tensile/Source/lib/source/ContractionSolution.cpp
+++ tensilelite/Tensile/Source/lib/source/ContractionSolution.cpp
@@ -701,9 +701,11 @@ namespace Tensile
                 args.template append<uint32_t>("bias_type",
                                                static_cast<uint32_t>(problem.bias().dataType()));
                 if(problemType.useBias)
+                {
                     args.template append<uint32_t>(
                         "strideBias",
-                        static_cast<uint32_t>(problem.useBias() ? bias.strides()[bias.dimensions() - 1] : 0)); // reserved
+                        static_cast<uint32_t>(problem.useBias()? bias.strides()[bias.dimensions() - 1]: 0)); // reserved
+                }
                 if(problemType.useBias == 3)
                 {
                     args.template append<uint32_t>(
