92f88155ee15d07cb16b3c27efce649b29032259
jichang <jimmy.chang@amd.com>
2024-05-17 02:30:56 +0000

    Make pure gemm can be search first prior to fused gemm

--- tensilelite/Tensile/SolutionLibrary.py
+++ tensilelite/Tensile/SolutionLibrary.py
@@ -256,8 +256,12 @@ class PredicateLibrary:
             else:
                 self.rows.append(row)
 
-        # Sort to ensure consistent fallback logic.
-        self.rows.sort(key=lambda x: x["predicate"])
+        if self.rows[0]["library"].tag == "Placeholder":
+            # Sort to ensure pure gemm can be search first
+            self.rows.sort(key=lambda x: len(x["library"].filenamePrefix))
+        else:
+            # Sort to ensure consistent fallback logic.
+            self.rows.sort(key=lambda x: x["predicate"])
 
     def remapSolutionIndices(self, indexMap):
         for row in self.rows:
