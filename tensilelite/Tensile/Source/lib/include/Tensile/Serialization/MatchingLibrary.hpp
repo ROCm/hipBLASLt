353309bfd8999e6084a0966ddb51a346421e3628
yenong-amd <92903026+yenong-amd@users.noreply.github.com>
2024-07-31 16:57:47 -0500

    Gridbased batch map (#930)
    * Add batch to GridBased library
    
    * Add batch points to FP16NN, FP16NT, FP16TN logic
    
    * Prints library logic index for TopSolutions
    
    * Add batch to grid distance
    
    * Add batch 16 plane
    
    * Enable kdtree search for batch
    
    ---------
    
    Co-authored-by: Yen Ong <yenong12@amd.com>

--- tensilelite/Tensile/Source/lib/include/Tensile/Serialization/MatchingLibrary.hpp
+++ tensilelite/Tensile/Source/lib/include/Tensile/Serialization/MatchingLibrary.hpp
@@ -59,7 +59,7 @@ namespace Tensile
                 if(!iot::outputting(io))
                 {
                     using Entry  = typename Table::Entry;
-                    using KEntry = typename Table::KEntry;
+                    using KBEntry = typename Table::KBEntry;
                     auto comp    = [](Entry const& e1, Entry const& e2) {
                         return e1.key < e2.key || (e1.key == e2.key && e1.speed > e2.speed);
                     };
@@ -73,15 +73,16 @@ namespace Tensile
                             for(auto it = table.table.begin(); it != table.table.end(); ++it)
                             {
                                 auto k   = it->key.size() > 3 ? it->key[3] : it->key[2];
+                                auto b   = it->key.size() > 3 ? it->key[2] : 1;
                                 auto key = std::tuple(it->key[0], it->key[1]);
                                 if(table.kSolutionMap.find(key) == table.kSolutionMap.end())
                                 {
-                                    std::vector<KEntry> v   = {KEntry(k, it->value)};
+                                    std::vector<KBEntry> v  = {KBEntry(k, b, it->value)};
                                     table.kSolutionMap[key] = v;
                                 }
                                 else
                                 {
-                                    table.kSolutionMap[key].push_back(KEntry(k, it->value));
+                                    table.kSolutionMap[key].push_back(KBEntry(k, b, it->value));
                                 }
                             }
 
