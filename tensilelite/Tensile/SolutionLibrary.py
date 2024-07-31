f9bd6092f136b08e6977f58e9a44f222f1d9dd01
yenong-amd <92903026+yenong-amd@users.noreply.github.com>
2024-07-31 16:57:47 -0500

    Gridbased batch map (#930)
    * Add batch to GridBased library
    
    * Add batch points to FP16NN, FP16NT, FP16TN logic
    
    * Prints library logic index for TopSolutions
    
    * Add batch to grid distance
    
    * Add batch 16 plane
    
    * Enable kdtree search for batch
    
    ---------
    
    Co-authored-by: Yen Ong <yenong12@amd.com>

--- tensilelite/Tensile/SolutionLibrary.py
+++ tensilelite/Tensile/SolutionLibrary.py
@@ -89,7 +89,7 @@ class MatchingLibrary:
             3: Properties.Property("FreeSizeB", index=0),
             1: Properties.Property("BoundSize", index=0)
         }
-        if distance == "Equality":
+        if distance == "Equality" or distance == "GridBased":
             propertyKeys[0] = Properties.Property("BatchSize", index=0)
 
         properties = list([propertyKeys[i] for i in indices if i in propertyKeys])
