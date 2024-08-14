99fee3fab6182b966ef99e40ba1891490fb434c2
Serge Lu <Serge.Lu@amd.com>
2024-08-15 01:24:02 +0800

    Added --dev-logic-filter to install.sh

--- install.sh
+++ install.sh
@@ -394,6 +394,7 @@ enable_gprof=false
 keep_build_tmp=false
 disable_hipblaslt_marker=false
 enable_tensile_marker=false
+logic_filter=
 
 
 rocm_path=/opt/rocm
@@ -408,7 +409,7 @@ fi
 # check if we have a modern version of getopt that can handle whitespace and long parameters
 getopt -T
 if [[ $? -eq 4 ]]; then
-  GETOPT_PARSE=$(getopt --name "${0}" --longoptions help,install,clients,dependencies,debug,hip-clang,static,relocatable,codecoverage,relwithdebinfo,address-sanitizer,merge-files,no-merge-files,no_tensile,no-tensile,msgpack,no-msgpack,logic:,cov:,fork:,branch:,test_local_path:,cpu_ref_lib:,build_dir:,use-custom-version:,architecture:,gprof,keep-build-tmp,legacy_hipblas_direct,disable-hipblaslt-marker,enable-tensile-marker --options hicdgrka:j:o:l:f:b:nu:t: -- "$@")
+  GETOPT_PARSE=$(getopt --name "${0}" --longoptions help,install,clients,dependencies,debug,hip-clang,static,relocatable,codecoverage,relwithdebinfo,address-sanitizer,merge-files,no-merge-files,no_tensile,no-tensile,msgpack,no-msgpack,logic:,cov:,fork:,branch:,test_local_path:,cpu_ref_lib:,build_dir:,use-custom-version:,architecture:,gprof,keep-build-tmp,legacy_hipblas_direct,disable-hipblaslt-marker,enable-tensile-marker,dev-logic-filter: --options hicdgrka:j:o:l:f:b:nu:t: -- "$@")
 else
   echo "Need a new version of getopt"
   exit 1
@@ -525,6 +526,9 @@ while true; do
         --enable-tensile-marker)
             enable_tensile_marker=true
             shift;;
+        --dev_logic_filter|--dev-logic-filter)
+            logic_filter=${2}
+            shift 2;;
         --) shift ; break ;;
         *)  echo "Unexpected command line parameter received: '${1}'; aborting";
             exit 1
@@ -754,6 +758,10 @@ pushd .
 
   if [[ "${build_release}" == false ]]; then
     tensile_opt="${tensile_opt} -DTensile_ASM_DEBUG=ON"
+  fi
+
+  if ! [[ "${logic_filter}" == "" ]]; then
+    tensile_opt="${tensile_opt} -DTensile_LOGIC_FILTER=${logic_filter}"
   fi
 
   if [[ "${enable_gprof}" == true ]]; then
