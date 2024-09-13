faa602349233203f9b6d22694507d03718ec16f5
fsx950223 <fsx950223@outlook.com>
2024-09-13 21:38:20 +0800

    add almalinux support

--- install.sh
+++ install.sh
@@ -59,9 +59,9 @@ supported_distro( )
     printf "supported_distro(): \$ID must be set\n"
     exit 2
   fi
-
+  
   case "${ID}" in
-    ubuntu|centos|rhel|fedora|sles|opensuse-leap|mariner|azurelinux)
+    ubuntu|centos|almalinux|rhel|fedora|sles|opensuse-leap|mariner|azurelinux)
         true
         ;;
     *)  printf "This script is currently supported on Ubuntu, CentOS, RHEL, Fedora, SLES, mariner and azurelinux\n"
@@ -193,7 +193,7 @@ install_packages( )
     fi
   fi
 
-  if [[ ( "${ID}" == "centos" ) || ( "${ID}" == "rhel" ) ]]; then
+  if [[ ( "${ID}" == "centos" ) || ( "${ID}" == "rhel" ) || ( "${ID}" == "almalinux" ) ]]; then
     if [[ "${VERSION_ID}" == "6" ]]; then
       library_dependencies_centos+=( "numactl" )
     else
@@ -225,7 +225,7 @@ install_packages( )
       pip3 install wheel
       ;;
 
-    centos|rhel)
+    centos|rhel|almalinux)
 #     yum -y update brings *all* installed packages up to date
 #     without seeking user approval
 #     elevate_if_not_root yum -y update
@@ -315,7 +315,7 @@ install_blis()
     #Download prebuilt AMD multithreaded blis
     if [[ ! -e "${build_dir}/deps/blis/lib/libblis.a" ]]; then
       case "${ID}" in
-          centos|rhel|sles|opensuse-leap)
+          centos|rhel|sles|opensuse-leap|almalinux)
               wget -nv -O blis.tar.gz https://github.com/amd/blis/releases/download/2.0/aocl-blis-mt-centos-2.0.tar.gz
               ;;
           ubuntu)
@@ -613,7 +613,7 @@ if [[ "${install_dependencies}" == true ]]; then
 
   # cmake is needed to install msgpack
   case "${ID}" in
-    centos|rhel|sles|opensuse-leap)
+    centos|rhel|sles|opensuse-leap|almalinux)
       if [[ "${tensile_msgpack_backend}" == true ]]; then
         install_msgpack_from_source
       fi
@@ -806,8 +806,8 @@ pushd .
       ubuntu)
         elevate_if_not_root dpkg -i hipblaslt[-\_]*.deb
       ;;
-      centos|rhel)
-        elevate_if_not_root yum -y localinstall hipblaslt-*.rpm
+      centos|rhel|almalinux)
+        elevate_if_not_root yum --allowerasing -y localinstall hipblaslt-*.rpm
       ;;
       fedora)
         elevate_if_not_root dnf install hipblaslt-*.rpm
