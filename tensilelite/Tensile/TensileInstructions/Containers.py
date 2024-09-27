093ad732032ef14952282b67cba988b5c60df6b3
cm chen <chingmin.chen@amd.com>
2024-09-27 00:53:48 -0700

    gfx12 - change to use byte_sel modifier for v_cvt_f32_fp8 and v_cvt_f32_bf8

--- tensilelite/Tensile/TensileInstructions/Containers.py
+++ tensilelite/Tensile/TensileInstructions/Containers.py
@@ -187,6 +187,7 @@ class SDWAModifiers(Container):
 class VOP3PModifiers(Container):
     op_sel:     Optional[List[int]] = None
     op_sel_hi:  Optional[List[int]] = None
+    byte_sel:   Optional[List[int]] = None
 
     def __post_init__(self):
         super().__init__()
@@ -197,6 +198,8 @@ class VOP3PModifiers(Container):
             l.append("op_sel:" + str(self.op_sel).replace(" ", ""))
         if self.op_sel_hi != None:
             l.append("op_sel_hi:" + str(self.op_sel_hi).replace(" ", ""))
+        if self.byte_sel != None:
+            l.append("byte_sel:" + str(self.byte_sel).replace(" ", ""))
         return l
 
     def __str__(self) -> str:
@@ -205,6 +208,8 @@ class VOP3PModifiers(Container):
             kStr += " op_sel:" + str(self.op_sel).replace(" ", "")
         if self.op_sel_hi != None:
             kStr += " op_sel_hi:" + str(self.op_sel_hi).replace(" ", "")
+        if self.byte_sel != None:
+            kStr += " byte_sel:" + str(self.byte_sel).replace(" ", "")
         return kStr
 
 class EXEC(Container):
