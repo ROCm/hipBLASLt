79403b5aca84430b0bcb2d1e15db7f08298d877b
Braden Stefanuk <121893577+bstefanuk@users.noreply.github.com>
2024-10-09 22:13:54 +0000

    feat: gfx942 variants via device ID and cu count"

---
+++ tensilelite/Tensile/Com/ArchVariant.py
@@ -0,0 +1,134 @@
+from pathlib import Path
+import re
+
+from typing import NamedTuple, Optional, List, Union, Tuple, Set
+
+from ..Common import print1
+
+class ArchVariant(NamedTuple):
+    Name: str
+    Gfx: str
+    DeviceIds: Optional[Set[str]] = None
+    CUCount: Optional[int] = None
+
+class LogicFileError(Exception):
+    def __init__(self, message="Expected line is either not present or is malformed"):
+        self.message = message
+        super().__init__(self.message)
+
+def extractArchVariant(file: Union[str, Path]) -> ArchVariant:
+    """Extracts an architecture variant from a given file.
+
+    The file is expected to have the following format:
+    - Line 1: Minimum required version (e.g., "- {MinimumRequiredVersion: 4.33.0}")
+    - Line 2: Name of the architecture variant (e.g., "- aquavanjaram")
+    - Line 3: Architecture and CUCount (e.g., "- {Architecture: gfx900, CUCount: 64}")
+    - Line 4: Device IDs (e.g., "- [Device 1234, Device 5678]")
+
+    Args:
+        file: Path to a logic file.
+
+    Returns:
+        ArchVariant: An object containing the extracted architecture variant.
+
+    Raises:
+        LogicFileError: If the file does not match the expected format.
+    """
+    def l0(line: str):
+        if not re.match(r'- \{MinimumRequiredVersion', line):
+            raise LogicFileError(f"Expected minimum required version: line: {line}")
+    
+    def l1(line: str):
+        return line[2:].strip()
+
+    def l2(line: str):
+        if match := re.match(r'- \{Architecture: (\w+), CUCount: (\d+)\}', line):
+            architecture, cu_count = match.groups()
+            return architecture, int(cu_count)
+        elif match := re.match(r'- gfx(\w+)', line):
+            return line[2:].strip(), None
+        else:
+            raise LogicFileError(f"Expected architecture and CU count, or only an archiecture: line: {line}")
+
+    def l3(line: str):
+        if re.match(r'- \[Device', line):
+            devices = re.findall(r'Device (\w+)', line)
+            return set(devices)
+        else:
+            raise LogicFileError(f"No device IDs found: line: {line}")
+
+    with open(file, "r") as f:
+        l0(f.readline())
+        name = l1(f.readline())
+        gfx, cu= l2(f.readline())
+        deviceIds = l3(f.readline())
+
+    return ArchVariant(Name=name, Gfx=gfx, DeviceIds=deviceIds, CUCount=cu)
+
+def parseArchVariantString(spec: str) -> Tuple[Set[str], Set[int]]:
+    """Parses an architecture variant specification to extract device IDs and CU counts.
+
+    An architecture variant specification must have the following form: "id=1234;cu=64".
+    Note that all entries must be separated by a semicolon.
+
+    Args:
+        spec: The specification string in the format "id=1234;cu=64;id=5678;cu=32".
+
+    Returns:
+        A tuple containing a set of device IDs and a set of CU counts. If an empty string 
+        is provided, empty sets will be returned.
+
+    Raises:
+        ValueError: If the specification string is invalid for the following reasons:
+            - The device ID is not a 4-digit number.
+            - The CU count is not a positive integer.
+    """
+    deviceIdLength = 4
+
+    deviceIds = set()
+    cuCounts = set()
+
+    if spec:
+        for s in spec.strip().split(";"):
+            key, _, value = s.partition("=")
+            value = value.strip()
+            if key == "id" and value.isdigit() and len(value) == deviceIdLength:
+                deviceIds.add(value)
+            elif key == "cu" and value.isdigit():
+                cuCounts.add(int(value))
+            else:
+                raise ValueError(f"Invalid architecture variant string: {spec}")
+
+    return deviceIds, cuCounts
+
+
+def matchArchVariant(gfxNames: Set[str], deviceIds: Set[str], cuCounts: Set[int], targetLogicFile: Union[str, Path]):
+    """Determines if the architecture variant specified in the target logic file matches the given criteria.
+
+    Args:
+        gfxNames: A set of valid GFX names to match against.
+        deviceIds: A set of valid device IDs to match against.
+        cuCounts: A set of valid CU counts to match against.
+        targetLogicFile: Path to the target logic file.
+
+    Returns:
+        bool: True if the architecture variant matches the given criteria, False otherwise.
+
+    Raises:
+        LineNotFoundError: If the target logic file does not contain the expected lines.
+        FileNotFoundError: If the target logic file does not exist.
+        ValueError: If the target logic file contains invalid entries.
+    """
+    # temporary, we shouldn't be including experimental files in the build anyway unless it's explicitly requested
+    if "experimental/" in str(targetLogicFile).lower():
+        return False
+
+    variant = extractArchVariant(targetLogicFile)
+    conditions = [
+        variant.Gfx in gfxNames,
+        any(id in deviceIds for id in variant.DeviceIds) or variant.DeviceIds == None,
+        variant.CUCount in cuCounts or variant.CUCount == None,
+    ]
+    if all(conditions):
+        return True
+    return False
--- tensilelite/Tensile/TensileCreateLibrary.py
+++ tensilelite/Tensile/TensileCreateLibrary.py
@@ -41,6 +41,7 @@ from .KernelWriterAssembly import KernelWriterAssembly
 from .SolutionLibrary import MasterSolutionLibrary
 from .SolutionStructs import Solution
 from .CustomYamlLoader import load_logic_gfx_arch
+from .Com.ArchVariant import ArchVariant, extractArchVariant, matchArchVariant, parseArchVariantString
 
 import argparse
 import collections
@@ -53,6 +54,7 @@ import shlex
 import shutil
 import subprocess
 import sys
+import functools
 from timeit import default_timer as timer
 from pathlib import Path
 from typing import Sequence, List
@@ -1262,6 +1264,7 @@ def TensileCreateLibrary():
   argParser.add_argument("--cmake-cxx-compiler",     dest="CmakeCxxCompiler",  action="store")
   argParser.add_argument("--code-object-version",    dest="CodeObjectVersion", choices=["default", "V4", "V5"], action="store")
   argParser.add_argument("--architecture",           dest="Architecture",      type=str, action="store", default="all", help="Supported archs: " + " ".join(architectureMap.keys()))
+  argParser.add_argument("--arch-variant",           dest="ArchVariant",       type=str, default=None, help="Architecture variant to build. All non-matching variants will be filtered out. Example: 'id=1234;cu=80'")
   argParser.add_argument("--merge-files",            dest="MergeFiles",        action="store_true")
   argParser.add_argument("--no-merge-files",         dest="MergeFiles",        action="store_false")
   argParser.add_argument("--num-merged-files",       dest="NumMergedFiles",    type=int, default=1, help="Number of files the kernels should be written into.")
@@ -1319,6 +1322,7 @@ def TensileCreateLibrary():
                         " Example: gfx942/Equality/* for building equality of gfx942 only")
 
   args = argParser.parse_args()
+
 
   logicPath = args.LogicPath
   outputPath = args.OutputPath
@@ -1415,6 +1419,14 @@ def TensileCreateLibrary():
   print1(f"# Experimental:      {args.Experimental}")
   if not args.Experimental:
     logicFiles = [file for file in logicFiles if "experimental" not in map(str.lower, Path(file).parts)]
+
+  if args.ArchVariant:
+    requestedGfxArchs = archs
+    requestedDeviceIds, requestedCuCounts = parseArchVariantString(args.ArchVariant)
+    print1(f"# Arch variant filter: gfx={requestedGfxArchs}, id={requestedDeviceIds}, cu={requestedCuCounts}")
+
+    fn = functools.partial(matchArchVariant, requestedGfxArchs, requestedDeviceIds, requestedCuCounts)
+    logicFiles = list(filter(fn, logicFiles))
 
   print1(f"# LibraryLogicFiles: {len(logicFiles)}")
   for logicFile in logicFiles:
