316d18e69e31d74220129b3f54c4622122fde87b
David Dixon <david.dixon@amd.com>
2024-10-29 14:20:02 -0500

    Update the hipblaslt lib path

--- library/src/amd_detail/rocblaslt/src/tensile_host.cpp
+++ library/src/amd_detail/rocblaslt/src/tensile_host.cpp
@@ -67,7 +67,7 @@
 #include <regex>
 #include <string_view>
 
-#define HIPBLASLT_LIB_PATH "/opt/rocm/hipblaslt/lib"
+#define HIPBLASLT_LIB_PATH "/opt/rocm/lib"
 
 #ifdef ENABLE_ROCTX
 #include <roctracer/roctx.h>
