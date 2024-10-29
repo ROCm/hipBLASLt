08c4f1fcb9e3408129d481320626e80612dbe8ba
YangWen Huang <yangwen.huang@amd.com>
2024-10-29 03:00:41 +0000

    Use more MIInstructions for fast mode

--- tensilelite/Tensile/Utilities/tensile_generator/tensile_config_generator.py
+++ tensilelite/Tensile/Utilities/tensile_generator/tensile_config_generator.py
@@ -1,3 +1,27 @@
+################################################################################
+#
+# Copyright (C) 2024 Advanced Micro Devices, Inc. All rights reserved.
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+#
+################################################################################
+
 import yaml
 import re
 import argparse
@@ -239,12 +263,13 @@ if args.hipblaslt_log and args.gridbase_config is None:
                 if mfma_instruction is None:
                     continue
                 matmul_instruction_gen = find_matmul_instruction(mfma_instruction, size, CU)
-                for matmul_instruction in matmul_instruction_gen:
+                total_inst = min(len(matmul_instruction_gen) // 3, 5)  # At least 5 insts and max of 33.3% of insts.
+                for index, matmul_instruction in enumerate(matmul_instruction_gen):
                     if matmul_instruction is not None:
                         if dtype_str not in matmul_instructions:
                             matmul_instructions[dtype_str] = dict()
                         matmul_instructions[dtype_str][str(matmul_instruction)] = matmul_instruction
-                        if args.fast:
+                        if args.fast and (index > total_inst):
                             break
     
                 if dtype_str in gemm_group:
