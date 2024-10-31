5384f3e15c1227dea731df533685fe5385e172a2
David Dixon <david.dixon@amd.com>
2024-10-31 11:24:44 -0500

    Ignore installed packages when installing tensilelite

--- cmake/virtualenv.cmake
+++ cmake/virtualenv.cmake
@@ -32,10 +32,10 @@ function(virtualenv_install)
     execute_process(
       COMMAND ${VIRTUALENV_BIN_DIR}/${VIRTUALENV_PYTHON_EXENAME} -m pip install --upgrade setuptools
     )
-    message("${VIRTUALENV_BIN_DIR}/${VIRTUALENV_PYTHON_EXENAME} -m pip install ${ARGN}")
+    message("${VIRTUALENV_BIN_DIR}/${VIRTUALENV_PYTHON_EXENAME} -m pip install --ignore-installed ${ARGN}")
     execute_process(
       RESULT_VARIABLE rc
-      COMMAND ${VIRTUALENV_BIN_DIR}/${VIRTUALENV_PYTHON_EXENAME} -m pip install ${ARGN}
+      COMMAND ${VIRTUALENV_BIN_DIR}/${VIRTUALENV_PYTHON_EXENAME} -m pip install --ignore-installed ${ARGN}
     )
     if(rc)
         message(FATAL_ERROR ${rc})
