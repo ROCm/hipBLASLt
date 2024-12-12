53244a7eefd15c63452a35677f1e76ee5cfae3ae
David Dixon <david.dixon@amd.com>
2024-12-12 17:06:47 +0000

    Merge conflict resolution

--- tensilelite/Tensile/TensileCreateLibrary.py
+++ tensilelite/Tensile/TensileCreateLibrary.py
@@ -373,13 +373,9 @@ def buildObjectFileNames(kernelWriterAssembly, kernels, kernelHelperObjs):
   if globalParameters["LazyLibraryLoading"]:
     fallbackLibs = list(set([kernel._state["codeObjectFile"] for kernel in kernels if "fallback" in kernel._state.get('codeObjectFile', "")]))
     sourceLibFiles += ["{0}_{1}.hsaco".format(name, arch) for name, arch in itertools.product(fallbackLibs, sourceArchs)]
-    if supportedCompiler(CxxCompiler):
-      sourceLibFiles += ["Kernels.so-000-%s.hsaco" % (arch) for arch in sourceArchs]
-  else: # Merge
-    if supportedCompiler(CxxCompiler):
-      sourceLibFiles += ["Kernels.so-000-%s.hsaco" % (arch) for arch in sourceArchs]
-    else:
-      raise RuntimeError("Unknown compiler {}".format(CxxCompiler))
+    sourceLibFiles += ["Kernels.so-000-%s.hsaco" % (arch) for arch in sourceArchs]
+  else:
+    sourceLibFiles += ["Kernels.so-000-%s.hsaco" % (arch) for arch in sourceArchs]
 
   # Returns names for all xnack versions
   def addxnack(name, ext):
