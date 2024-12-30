4f1bdd1d1fc4aa610fc51e8493b8a4f0aadd93a8
jichang <jimmy.chang@amd.com>
2024-12-30 07:00:40 +0000

    Fix: deps folder is incorrect
    [ROCm/hipBLASLt commit: 5400c61b7ece89d83c964e08ce491c462d481f54]

--- install.sh
+++ install.sh
@@ -671,7 +671,7 @@ if [[ "${install_dependencies}" == true ]]; then
   pushd .
     printf "\033[32mBuilding \033[33mgoogletest\033[32m from source; installing into \033[33m/usr/local\033[0m\n"
     mkdir -p ${build_dir}/deps && cd ${build_dir}/deps
-    ${cmake_executable} ${build_dir}/deps
+    ${cmake_executable} ${root_path}/deps
     make -j$(nproc)
     elevate_if_not_root make install
   popd
