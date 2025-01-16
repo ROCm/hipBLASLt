00e3bcb4ac5e04c0a9333045ded60d7f1dac2255
Pieter Ghysels <pieter.ghysels@amd.com>
2025-01-16 19:04:19 +0000

    Do not assume padding for the last block in the batch

--- clients/common/hipblaslt_init_device.cpp
+++ clients/common/hipblaslt_init_device.cpp
@@ -42,7 +42,7 @@ __global__ void fill_kernel(T* A, size_t size, size_t offset, F f)
 template <typename T, typename F>
 void fill_batch(T* A, size_t M, size_t N, size_t lda, size_t stride, size_t batch_count, const F& f)
 {
-    size_t size_64 = std::max(lda * N, stride) * batch_count;
+    size_t size_64 = lda * N + size_t(batch_count - 1) * stride;
     constexpr size_t c_i32_max = size_t(std::numeric_limits<int32_t>::max());
     for(size_t offset = 0; offset < size_64; offset += c_i32_max)
     {
