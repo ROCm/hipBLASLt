2c2b4e78396dcbd930fe7594802f3fc642285a5c
Ethan <Yi-Chen.Lin@amd.com>
2025-01-17 12:16:59 +0800

    code-gen: Allowed WaveGroups be distributed along n-dim for DTVA/SwizzledA
    * Allow WaveGroup in N-dim for swizzledA
    
    * directly modify totalElementsCoal/PerpA
    
    * restore assertion and rejection
    
    * Added DTVB & updated pytests
    
    * Fix TLU=True case
    
    [ROCm/hipBLASLt commit: 38efb62a7bad945dc41535e4ce9fe0f5e3530996]

--- tensilelite/Tensile/KernelWriterAssembly.py
+++ tensilelite/Tensile/KernelWriterAssembly.py
@@ -3363,7 +3363,9 @@ class KernelWriterAssembly(KernelWriter):
         tmpSgpr = tmpSgprInfo.idx
         # Calc numKr, TODO- 32 should be MI_K * 2
         module.add(SLShiftRightB32(dst=sgpr(tmpSgpr), shiftHex=hex(log2(32)), src=sgpr("SizesSum"),  comment="SWZ: numKr = DimK / 32"))
-        module.add(VMulU32U24(dst=vgpr(qReg), src0=sgpr(tmpSgpr), src1=vgpr(qReg), comment="SWZ: wave-id *= numKr"))
+        WvG_M = kernel["MIWaveGroup"][0]
+        module.add(VAndB32(dst=vgpr(qReg), src0=hex(WvG_M-1), src1=vgpr(qReg), comment="SWZ: wave_id (along_M) %= MIWG[0]"))
+        module.add(VMulU32U24(dst=vgpr(qReg), src0=sgpr(tmpSgpr), src1=vgpr(qReg), comment="SWZ: wave_id (along_M) *= numKr"))
     elif isDTVAB:
       # offset calculation for DirectToVgpr
       # call function from LraTileAssignmentMFMA for DirectToVgpr
