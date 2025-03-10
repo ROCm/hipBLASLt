d1fc126ba8a21a7325f70c5715344ac8dcc440d0
Anush Elangovan <anush.elangovan@amd.com>
2025-03-09 22:28:35 -0700

    [hipblasLT]: Add GFX1103, GFX1150, GFX1151
    Add the framework for gfx1103 (PHX), gfx1150 (STX) and
    gfx1151(STXHALO)
    
    Copy the gfx1102 kernels as baseline. They seem to work ok when
    tested on gfx115X. Once baseline is in we should:
    
    1) tensilelite/Tensile/TensileInstructions/*.py with changes
    2) Verify BUFFER_RESOURCE_3RD_DWORD is correct in memory_gfx.h
    3) Update tensilelite/Tensile/SolutionStructs.py and
       tensilelite/Tensile/KernelWriter.py
       tensilelite/Tensile/KernelWriterAssembly.py
       tensilelite/Tensile/Components/[LocalRead.py | GlobalWriteBatch.py]
    4) Run tuning and benchmark with hipblasLT-bench
    
    TEST: Build TheRock with gfx1150
    
     cmake -B build -GNinja . -DTHEROCK_AMDGPU_TARGETS=gfx1151
     cmake --build build

--- tensilelite/Tensile/Common.py
+++ tensilelite/Tensile/Common.py
@@ -222,7 +222,7 @@ globalParameters["MergeFiles"] = True             # F=store every solution and k
 globalParameters["NumMergedFiles"] = 1            # The number of files that kernels should be split between when merging
 
 globalParameters["MaxFileName"] = 64              # If a file name would be longer than this, shorten it with a hash.
-globalParameters["SupportedISA"] = [(8,0,3), (9,0,0), (9,0,6), (9,0,8), (9,0,10), (9,4,0), (9,4,1), (9,4,2), (10,1,0), (10,1,1), (10,1,2), (10,3,0), (11,0,0), (11,0,1), (11,0,2), (12,0,0), (12,0,1)] # assembly kernels writer supports these architectures
+globalParameters["SupportedISA"] = [(8,0,3), (9,0,0), (9,0,6), (9,0,8), (9,0,10), (9,4,0), (9,4,1), (9,4,2), (10,1,0), (10,1,1), (10,1,2), (10,3,0), (11,0,0), (11,0,1), (11,0,2), (11,0,3), (11,5,0), (11,5,1), (12,0,0), (12,0,1)] # assembly kernels writer supports these architectures
 
 globalParameters["NewClient"] = 2                                 # Old client deprecated: NewClient must be set to 2.
 globalParameters["ClientBuildPath"] = "0_Build"                   # subdirectory for host code build directory
@@ -308,7 +308,8 @@ architectureMap = {
   'gfx941':'aquavanjaram', 'gfx941:xnack+':'aquavanjaram', 'gfx941:xnack-':'aquavanjaram',
   'gfx942':'aquavanjaram', 'gfx942:xnack+':'aquavanjaram', 'gfx942:xnack-':'aquavanjaram',
   'gfx1010':'navi10', 'gfx1011':'navi12', 'gfx1012':'navi14', 'gfx1030':'navi21',
-  'gfx1100':'navi31', 'gfx1101':'navi32', 'gfx1102':'navi33',
+  'gfx1100':'navi31', 'gfx1101':'navi32', 'gfx1102':'navi33', 'gfx1103':'gfx1103',
+  'gfx1150':'gfx1150', 'gfx1151':'gfx1151',
   'gfx1200':'gfx1200', 'gfx1201':'gfx1201',
 }
 
