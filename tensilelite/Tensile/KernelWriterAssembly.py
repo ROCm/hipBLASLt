9eac38f1a03a52856314de7577f383092393a9cb
Mihail <mgeorgie@banff-cyxtera-s81-1.ctr.dcgpu>
2025-03-16 18:08:34 +0000

    Small bracket polish

--- tensilelite/Tensile/KernelWriterAssembly.py
+++ tensilelite/Tensile/KernelWriterAssembly.py
@@ -11062,23 +11062,23 @@ class KernelWriterAssembly(KernelWriter):
           mubuf2 = MUBUFModifiers(offen=True, offset12=int(offset + bpl/4), glc=glc, slc=slc, nt=nt, lds=lds)
           dst2 = destVgpr + "+" + str(int(rpv//4)) if isinstance(destVgpr, str) else int(destVgpr + int(rpv//4))
 
-          dst = vgpr(dst2, rpv//4)          
+          dst = vgpr(dst2, rpv//4)
           rv.add(BufferLoadB128(dst=dst, vaddr=addr0, saddr=addr1, \
                                 soffset=soffset, mubuf=mubuf2, comment=comment))
-          #+0.5          
+          #+0.5
           mubuf3 = MUBUFModifiers(offen=True, offset12=int(offset + bpl/2), glc=glc, slc=slc, nt=nt, lds=lds)
           dst3 = destVgpr + "+" + str(int(rpv//2)) if isinstance(destVgpr, str) else int(destVgpr + int(rpv//2))
           
-          dst = vgpr(dst3, rpv//4)          
+          dst = vgpr(dst3, rpv//4)
           rv.add(BufferLoadB128(dst=dst, vaddr=addr0, saddr=addr1, \
                                 soffset=soffset, mubuf=mubuf3, comment=comment))
           #+0.75
-          mubuf4 = MUBUFModifiers(offen=True, offset12= int(offset + 3*(bpl/4)), glc=glc, slc=slc, nt=nt, lds=lds)          
-          dst4 = destVgpr + "+" + str(3*int(rpv//4)) if isinstance(destVgpr, str) else int(destVgpr + 3*int(rpv//4))         
+          mubuf4 = MUBUFModifiers(offen=True, offset12= int(offset + 3*bpl/4), glc=glc, slc=slc, nt=nt, lds=lds)
+          dst4 = destVgpr + "+" + str(3*int(rpv//4)) if isinstance(destVgpr, str) else int(destVgpr + 3*int(rpv//4))
           
           dst = vgpr(dst4, rpv//4)
           rv.add(BufferLoadB128(dst=dst, vaddr=addr0, saddr=addr1, \
-                                soffset=soffset, mubuf=mubuf4, comment=comment))        
+                                soffset=soffset, mubuf=mubuf4, comment=comment))
         else:
           assert 0, "%s\nchooseGlobalRead: bad bpl %u"%(self.states.kernelName,bpl)
 
