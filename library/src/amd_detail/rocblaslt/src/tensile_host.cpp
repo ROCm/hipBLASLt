9c57d21501a1d417efe59057d3d287c92869f8ca
Braden Stefanuk <121893577+bstefanuk@users.noreply.github.com>
2025-04-07 16:54:23 -0600

    Use proper logging for libpath printing (#1877)

--- library/src/amd_detail/rocblaslt/src/tensile_host.cpp
+++ library/src/amd_detail/rocblaslt/src/tensile_host.cpp
@@ -1913,8 +1913,12 @@ namespace
             const char* env = getenv("HIPBLASLT_TENSILE_LIBPATH");
             if(env)
             {
-                std::cout << "rocblaslt info: Using HIPBLASLT_TENSILE_LIBPATH=" << env
-                          << std::endl;
+                if(get_logger_layer_mode() & rocblaslt_layer_mode_log_info)
+                {
+                    std::ostringstream msg;
+                    msg << "Using HIPBLASLT_TENSILE_LIBPATH=" << env << std::endl;
+                    log_info(__func__, msg.str());
+                }
                 path = env;
             }
             else
@@ -1942,8 +1946,12 @@ namespace
                 if(TestPath(path + "/" + processor))
                     path += "/" + processor;
 
-                std::cout << "rocblaslt info: HIPBLASLT_TENSILE_LIBPATH not set: Using " << path 
-                          << std::endl;
+                if(get_logger_layer_mode() & rocblaslt_layer_mode_log_info)
+                {
+                    std::ostringstream msg;
+                    msg << "HIPBLASLT_TENSILE_LIBPATH not set: Using " << path << std::endl;
+                    log_info(__func__, msg.str());
+                }
             }
 
             // only load modules for the current architecture
