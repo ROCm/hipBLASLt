9828fe715a89413494e5c2ce4508526063cb66a1
Mihail <mgeorgie@banff-cyxtera-s81-1.ctr.dcgpu>
2025-04-10 12:49:34 +0000

    Added empty string, to restart the checks

--- tensilelite/Tensile/TensileInstructions/Pass.py
+++ tensilelite/Tensile/TensileInstructions/Pass.py
@@ -18,7 +18,7 @@
 # COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
 # IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNE-
 # CTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-################################################################################
+#################################################################################
 
 from rocisa.instruction import BranchInstruction, CommonInstruction, Instruction, \
                           CompositeInstruction, MacroInstruction, \
