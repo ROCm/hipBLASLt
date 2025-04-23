5c3b4db2fd8c0ab4829ae1df9db8c7c6da06ceb5
Braden Stefanuk <121893577+bstefanuk@users.noreply.github.com>
2025-04-23 18:49:37 +0000

    chore: use ENABLE_BLIS with path searching

--- next-cmake/cmake/FindBLIS.cmake
+++ next-cmake/cmake/FindBLIS.cmake
@@ -20,6 +20,11 @@
 # THE SOFTWARE.
 #
 # ########################################################################
+# include(FindPackageHandleStandardArgs)
+
+# set(BLIS_PATH_4_2_0 "/opt/AMD/aocl/aocl-linux-gcc-4.2.0/gcc/lib_ILP64/")
+# set(BLIS_PATH_4_1_0 "/opt/AMD/aocl/aocl-linux-aocc-4.1.0/aocc/lib_ILP64/")
+# set(BLIS_PATH_4_0   "/opt/AMD/aocl/aocl-linux-aocc-4.0/lib_ILP64/")
 
 if(EXISTS          "/opt/AMD/aocl/aocl-linux-gcc-4.2.0/gcc/lib_ILP64/libblis-mt.a" )
     set( BLIS_LIB /opt/AMD/aocl/aocl-linux-gcc-4.2.0/gcc/lib_ILP64/libblis-mt.a )
@@ -42,6 +47,49 @@ elseif(EXISTS      "/usr/local/lib/libblis.a")
 else()
     message(FATAL_ERROR "BLIS lib not found.")
 endif()
+
+# find_path(BLIS_INCLUDE_DIR
+#     NAMES blis.h
+#     PATHS
+#         ${BLIS_ROOT}
+#         ENV BLIS_ROOT
+#         ${BLIS_PATH_4_2_0}
+#         ${BLIS_PATH_4_1_0}
+#         ${BLIS_PATH_4_0}
+#     NO_DEFAULT_PATH
+#     PATH_SUFFIXES include
+# )
+
+# find_library(BLIS_LIB
+#     NAMES blis blis-mt
+#     PATHS
+#         ${BLIS_ROOT}
+#         ENV BLIS_ROOT
+#         ${BLIS_PATH_4_2_0}
+#         ${BLIS_PATH_4_1_0}
+#         ${BLIS_PATH_4_0}
+#     NO_DEFAULT_PATH
+#     PATH_SUFFIXES lib lib_ILP64
+# )
+
+# find_package_handle_standard_args(BLIS
+#     REQUIRED_VARS BLIS_LIB BLIS_INCLUDE_DIR
+#     REQUIRED_MSG "BLIS not found. Please set the BLIS_DIR variable to the directory containing the BLIS installation."
+# )
+
+# if(BLIS_FOUND)
+#     set(BLIS_LIBRARIES ${BLIS_LIB})
+#     set(BLIS_INCLUDE_DIRS ${BLIS_INCLUDE_DIR})
+
+#     # Create an imported target for BLIS
+#     if(NOT TARGET BLIS::BLIS)
+#         add_library(BLIS::BLIS UNKNOWN IMPORTED)
+#         set_target_properties(BLIS::BLIS PROPERTIES
+#             IMPORTED_LOCATION "${BLIS_LIBRARY}"
+#             INTERFACE_INCLUDE_DIRECTORIES "${BLIS_INCLUDE_DIR}"
+#         )
+#     endif()
+# endif()
 
 set(BLIS_FOUND TRUE)
 set(BLIS_INCLUDE_DIR ${BLIS_INCLUDE_DIR})
