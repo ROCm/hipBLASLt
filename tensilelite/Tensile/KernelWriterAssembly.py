9222275a1d44198afb5e3b78a1838c22798e9036
Peter Cheng <Peter.Cheng@amd.com>
2025-04-25 11:01:28 +0800

    fix lds padding vgpr alignment
    [ROCm/hipBLASLt commit: 453d3e7cad2a171cadbefd338ae870244b2ddf3d]

--- tensilelite/Tensile/KernelWriterAssembly.py
+++ tensilelite/Tensile/KernelWriterAssembly.py
@@ -3834,7 +3834,7 @@ class KernelWriterAssembly(KernelWriter):
 
     # LdsBlockSizePerPad: add padding
     if kernel["LdsBlockSizePerPad%s"%tc] != 0 and kernel["LdsPad%s"%tc] != 0:
-      tmpVgpr = self.vgprPool.checkOut(1)
+      tmpVgpr = self.vgprPool.checkOutAligned(2, 2)
       tmpVgprRes = ContinuousRegister(tmpVgpr, 2)
       module.add(vectorStaticDivide(tmpVgpr, destVgpr, kernel["LdsBlockSizePerPad%s"%tc], tmpVgprRes, \
         "padding %u per block %u" % (kernel["LdsPad%s"%tc] * tP["bpeDS"], kernel["LdsBlockSizePerPad%s"%tc])))
