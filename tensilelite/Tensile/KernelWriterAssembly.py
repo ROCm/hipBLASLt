619364e5bab66f8fea14e37e72e27ece66189000
mahmoodw <wmahmood@amd.com>
2025-06-04 22:34:20 +0000

    changing sgpr limits

--- tensilelite/Tensile/KernelWriterAssembly.py
+++ tensilelite/Tensile/KernelWriterAssembly.py
@@ -11612,10 +11612,13 @@ class KernelWriterAssembly(KernelWriter):
     GSUtotal = max(2,GSUtotal)
     return GSUtotal
 
-  def setOccupancy(self, kernel):
-    # Use VGPR up to next occupancy threshold:
-    maxVgprs, occupancy = self.getMaxRegsForOccupancy(kernel["NumThreads"], self.vgprPool.size(), self.sgprPool.size(), \
-                                                      self.getLdsSize(kernel), self.agprPool.size(), self.states.doubleVgpr)
+    def setOccupancy():
+      # Use VGPR up to next occupancy threshold:
+      # Account for additional temp sgprs that will be required for code gen, up to physical limits. +5 approximates upper end of required temp space for GSU sync
+      requiredSgprs = min(self.sgprPool.size() + 5, self.states.regCaps["MaxSgpr"])
+
+      maxVgprs, occupancy = self.getMaxRegsForOccupancy(kernel["NumThreads"], self.vgprPool.size(), requiredSgprs, \
+                                                        self.getLdsSize(kernel), self.agprPool.size(), self.states.doubleVgpr)
     # Set occupancy limit for register pools
     # TODO: Support gfx12
     if kernel["ISA"][0] != 12:
