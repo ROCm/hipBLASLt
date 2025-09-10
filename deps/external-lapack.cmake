ba4bf32c7774e898b7c8d38ee6b3166443ff8292
David Dixon <165835255+davidd-amd@users.noreply.github.com>
2025-09-10 14:09:47 -0600

    netlib lapack version (#1539)
    ## Motivation
    
    3.7.1 is unfindable in most ubuntu verisons we've tested as cheev_
    symbol appears to be missing.
    
    ## Technical Details
    
    - Pulls 3.9.1 which aligns with the mathci docker file and succeeds when
    configuring.

--- deps/external-lapack.cmake
+++ deps/external-lapack.cmake
@@ -28,7 +28,7 @@ set( lapack_cmake_args -DCMAKE_INSTALL_PREFIX=${PREFIX_LAPACK} -DBUILD_SHARED_LI
 append_cmake_cli_arguments( lapack_cmake_args lapack_cmake_args )
 
 set( lapack_git_repository "https://github.com/Reference-LAPACK/lapack-release" CACHE STRING "URL to download lapack from" )
-set( lapack_git_tag "lapack-3.7.1" CACHE STRING "git branch" )
+set( lapack_git_tag "lapack-3.9.1" CACHE STRING "git branch" )
 
 # message( STATUS "lapack_make ( " ${lapack_make} " ) " )
 # message( STATUS "lapack_cmake_args ( " ${lapack_cmake_args} " ) " )
