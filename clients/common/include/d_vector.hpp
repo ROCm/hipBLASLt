08b141c114cfa934e9f93b83ca0719a9bffecbae
Naveen Kumar Elumalai <69491510+NaveenElumalaiAMD@users.noreply.github.com>
2025-10-06 12:51:35 -0600

    [hipBLASLt] Improve device memory allocation failure handling in hipBLASLt client (#1928)
    ## Motivation
    
    To stop tests from aborting due to false failure.
    
    ## Technical Details
    
    1. Reset the HIP error code after the first allocation if it fails.
    2. Catch the HIP error if it fails even after second allocation attempt
    in testing_matmul.hpp

--- clients/common/include/d_vector.hpp
+++ clients/common/include/d_vector.hpp
@@ -97,7 +97,7 @@ public:
         char* d = nullptr;
         if((use_HMM ? hipMallocManaged(&d, capacity) : hipMalloc(&d, capacity)) != hipSuccess)
         {
-            hipblaslt_cerr << "Error allocating (" << (m_size >> 30) << " GB) device memory"
+            hipblaslt_cerr << "Insufficient memory to  allocate (" << (m_size >> 30) << " GB) in device "
                            << std::endl;
             d      = nullptr;
             m_size = m_capacity = 0;
@@ -171,6 +171,16 @@ public:
         Instance().restore(dm);
     }
 
+    // Helper: get total device mem in bytes
+    size_t total_device_mem()
+    {
+        int device;
+        (void)hipGetDevice(&device);
+        hipDeviceProp_t prop;
+        (void)hipGetDeviceProperties(&prop, device);
+        return static_cast<size_t>(prop.totalGlobalMem);
+    }
+
 private:
     std::vector<M> m_pool, m_pool_managed;
 
@@ -185,7 +195,7 @@ private:
         auto& pool = use_HMM ? m_pool_managed : m_pool;
         auto  it   = std::lower_bound(pool.begin(), pool.end(), bytes);
         if(it != pool.end() && // found a buffer that is large enough ..
-           it->capacity() < 4 * bytes) // but not way too large
+           it->capacity() < 2 * bytes) // but not way too large
         {
             auto p = std::move(*it);
             p.resize(bytes);
@@ -194,16 +204,29 @@ private:
         }
         else
         {
+            // Threshold for "huge" allocations — skip 20% extra allocation 
+            static const size_t big_thresh = total_device_mem() / 4;
+            bool huge_request = (bytes >= big_thresh);
+
             // remove the (largest) buffer that was too small
             if(it != pool.begin())
                 pool.erase(it - 1);
-            // Allocate 20% extra for later reuse
-            auto e = M(bytes, bytes * 1.2, use_HMM);
+
+            // Allocate 20% extra if it is not huge_request for later reuse
+            size_t alloc_capacity = huge_request ? bytes : static_cast<size_t>(bytes * 1.2); 
+
+            auto e = M(bytes, alloc_capacity, use_HMM);
             if(e.get())
                 return e;
-            hipblaslt_cerr << "Clearing memory pool" << std::endl;
+            hipblaslt_cerr << "Clearing memory pool and retrying" << std::endl;
             // allocation failed, so clear the pool and try again (without the 20%)
             pool.clear();
+
+            // reset the error code from previous hipMalloc failure and try again to allocate memory
+            hipError_t err = hipPeekAtLastError();
+            if(err == hipErrorOutOfMemory || err == hipErrorMemoryAllocation )
+                (void)hipGetLastError();
+
             return M(bytes, bytes, use_HMM);
         }
     }
