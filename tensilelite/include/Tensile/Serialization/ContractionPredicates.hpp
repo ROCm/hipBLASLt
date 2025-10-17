d3e61ba413d85c2394c4cdaa0dac897b79fbe882
KKyang <2676255+KKyang@users.noreply.github.com>
2025-10-17 16:23:00 +0000

    [rocm-libraries] ROCm/rocm-libraries#1994 (commit f86429d)
    Revert "[hipBLASlt] Add predicate for PredictionLibrary"
     (#1994)
    
    Reverts ROCm/rocm-libraries#1618
    We can't solve conflict by changing FreeSize to Prediction.

--- tensilelite/include/Tensile/Serialization/ContractionPredicates.hpp
+++ tensilelite/include/Tensile/Serialization/ContractionPredicates.hpp
@@ -105,7 +105,6 @@ namespace TensileLite
                      Base::template Pair<Predicates::Contraction::EqualityMatching>(),
                      Base::template Pair<Predicates::Contraction::RangeMatching>(),
                      Base::template Pair<Predicates::Contraction::FreeSizeMatching>(),
-                     Base::template Pair<Predicates::Contraction::PredictionMatching>(),
                      Base::template Pair<Predicates::Contraction::UseGradientEqual>(),
                      Base::template Pair<Predicates::Contraction::ActivationCheck>(),
                      Base::template Pair<Predicates::Contraction::ActivationComputeTypeEqual>(),
@@ -416,12 +415,6 @@ namespace TensileLite
         template <typename IO>
         struct MappingTraits<Predicates::Contraction::FreeSizeMatching, IO>
             : public AutoMappingTraits<Predicates::Contraction::FreeSizeMatching, IO>
-        {
-        };
-
-        template <typename IO>
-        struct MappingTraits<Predicates::Contraction::PredictionMatching, IO>
-            : public AutoMappingTraits<Predicates::Contraction::PredictionMatching, IO>
         {
         };
 
