ac50f8618c89db59c03049de906354f4e4cf0a6e
darrenhsieh-amd <235250987+darrenhsieh-amd@users.noreply.github.com>
2025-10-28 12:44:47 +0000

    [rocm-libraries] ROCm/rocm-libraries#2234 (commit 29acce9)
    [hipblaslt] Add missing copyright headers for hipBLASLt
     files.

--- utilities/QuickTune/tuning.sh
+++ utilities/QuickTune/tuning.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
+# Copyright (C) Advanced Micro Devices, Inc., or its affiliates.
+# SPDX-License-Identifier:  MIT
+
 nohup python gemm_tuning.py --input_file example/Qwen3-32B_hipblaslt.log --output_path test_tuning --requested_solution 128 > output.log 2>&1 &
