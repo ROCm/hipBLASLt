100c2c22b86d4713cadc59d44dc790d4929b6c88
Josh Chang <josh.chang@amd.com>
2025-11-04 09:59:59 +0800

    Switch MbskPrefetchMethod between 0/1 based on # store elements. (#2423)
    ## Motivation
    
    <!-- Explain the purpose of this PR and the goals it aims to achieve.
    -->
    Enhance performance on small MT big K problem sizes.
    
    ## Technical Details
    
    <!-- Explain the changes along with any relevant GitHub links. -->
    Switch MbskPrefetchMethod between 0/1 based on # store elements.
    
    ## Test Plan
    
    <!-- Explain any relevant testing done to verify this PR. -->
    hipblaslt-test and tensilelite test pass
    
    ## Test Result
    
    <!-- Briefly summarize test outcomes. -->
    hipblaslt-test and tensilelite test pass
    
    ## Submission Checklist
    
    - [ ] Look over the contributing guidelines at
    https://github.com/ROCm/ROCm/blob/develop/CONTRIBUTING.md#pull-requests.

--- tensilelite/Tensile/Common/GlobalParameters.py
+++ tensilelite/Tensile/Common/GlobalParameters.py
@@ -422,7 +422,7 @@ defaultBenchmarkCommonParameters = [
     {"ForceDisableShadowInit": [False]},
     {"LDSTrInst": [False]},
     {"WaveSplitK": [ False ]},
-    {"MbskPrefetchMethod": [0]},
+    {"MbskPrefetchMethod": [-1]},
     {"UseCustomMainLoopSchedule": [1]},
     {"SpaceFillingAlgo": [[]]},
     {"SFCWGM": [[[1,1],[1,1]]]}
