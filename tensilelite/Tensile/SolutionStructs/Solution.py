21ae8c8db6d52fccce1a889c51b43af5ede11dde
Leo Wu <5629343+leowu2017@users.noreply.github.com>
2026-06-02 03:12:46 +0000

    [rocm-libraries] ROCm/rocm-libraries#7790 (commit 49fe8a7)
    [hipSPARSELt] Suppress rejection on first attempt to expand
     LRVWM (#7790)
    
    [AIHPSPAR-104](https://amd-hub.atlassian.net/browse/AIHPSPAR-104)
    
    ## Motivation
    
    When running install.sh for hipSPARSELt, multiple log messages like the
    following are printed:
    ```
    reject: totalVectorsMetadata 192 % NumThreads 128 != 0
    reject: totalVectorsMetadata 384 % NumThreads 256 != 0
    ```
    These messages are repeatedly emitted during attempts to expand LRVWM.
    However, they do not represent actual rejections. If the expansion
    fails, a fallback solution is executed. Therefore, printing these
    messages during the initial expansion attempts is unnecessary and
    misleading.
    
    ## Technical Details
    
    Suppress rejection messages on the first attempt. Only print the
    rejection message if the fallback attempt also fails.
    
    ## Test Plan
    
    Run `install.sh` and verify the presence of rejection messages.
    
    ## Test Result
    
    No rejection messages are observed.
    
    ## Submission Checklist
    
    - [x] Look over the contributing guidelines at
    https://github.com/ROCm/ROCm/blob/develop/CONTRIBUTING.md#pull-requests.
    
    [AIHPSPAR-104]:
    https://amd-hub.atlassian.net/browse/AIHPSPAR-104?atlOrigin=eyJpIjoiNWRkNTljNzYxNjVmNDY3MDlhMDU5Y2ZhYzA5YTRkZjUiLCJwIjoiZ2l0aHViLWNvbS1KU1cifQ

--- tensilelite/Tensile/SolutionStructs/Solution.py
+++ tensilelite/Tensile/SolutionStructs/Solution.py
@@ -2428,10 +2428,13 @@ class Solution(collections.abc.Mapping):
       bGlobalReadVectorWidthMetadata = state["GlobalReadVectorWidthMetadata"]
       glvwMlimit = 16
       if state["GlobalReadVectorWidthMetadata"] < glvwMlimit:
+        # If SolutionIndex is present and non-negative, this means we are during TensileCreateLibrary stage
+        # Don't print rejection reason for the first attempt to expand GRVWM.
+        _printRejectionReason = (state.get("SolutionIndex", -1) == -1) and printRejectionReason
         if state["ProblemType"]["Sparse"] == 2:
           GlobalReadVectorWidth = min(state["GlobalReadVectorWidthMetadata"] * state["NumLoadsPerpendicularB"], depthUM, glvwMlimit) #sum all need read
           tvm = totalElementsM // GlobalReadVectorWidth
-          if not Solution.setGlobalReadVectorWidth(state, "Metadata", tvm, GlobalReadVectorWidth, printRejectionReason):
+          if not Solution.setGlobalReadVectorWidth(state, "Metadata", tvm, GlobalReadVectorWidth, _printRejectionReason):
             #fallback
             tvm = totalElementsM // bGlobalReadVectorWidthMetadata
             Solution.setGlobalReadVectorWidth(state, "Metadata", tvm, bGlobalReadVectorWidthMetadata, printRejectionReason)
@@ -2440,11 +2443,10 @@ class Solution(collections.abc.Mapping):
           if GlobalReadVectorWidthMetadata == 0:
             GlobalReadVectorWidthMetadata = 1
           totalVectorsCoalescedM = totalElementsCoalescedM // GlobalReadVectorWidthMetadata
-          totalVectorsM = totalElementsM // GlobalReadVectorWidthMetadata
         else:
           GlobalReadVectorWidth = min(state["GlobalReadVectorWidthMetadata"] * state["NumLoadsPerpendicularA"], depthUM, glvwMlimit) #sum all need read
           tvm = totalElementsM // GlobalReadVectorWidth
-          if not Solution.setGlobalReadVectorWidth(state, "Metadata", tvm, GlobalReadVectorWidth, printRejectionReason):
+          if not Solution.setGlobalReadVectorWidth(state, "Metadata", tvm, GlobalReadVectorWidth, _printRejectionReason):
             #fallback
             tvm = totalElementsM // bGlobalReadVectorWidthMetadata
             Solution.setGlobalReadVectorWidth(state, "Metadata", tvm, bGlobalReadVectorWidthMetadata, printRejectionReason)
@@ -2453,7 +2455,6 @@ class Solution(collections.abc.Mapping):
           if GlobalReadVectorWidthMetadata == 0:
             GlobalReadVectorWidthMetadata = 1
           totalVectorsCoalescedM = totalElementsCoalescedM // GlobalReadVectorWidthMetadata
-          totalVectorsM = totalElementsM // GlobalReadVectorWidthMetadata
 
       if not Solution.setGlobalLoadTileDimClassic(state, "Metadata", state["NumLoadsMetadata"], \
           totalVectorsCoalescedM, totalElementsPerpM, depthUM, printRejectionReason):
